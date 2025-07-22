bd1706714e8af61c1229855806d994f13cb1b1e6
Mayanksinghhh <128398164+Mayanksinghhh@users.noreply.github.com>
2025-07-22 21:10:53 +0530

    Update ai.service.js

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI("AIzaSyBMQMoV3e_mHs_2qDb5b2rOURQl1pmdUjM");
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
 const model = genAI.getGenerativeModel({
     model: "gemini-2.0-flash",
     systemInstruction: `
